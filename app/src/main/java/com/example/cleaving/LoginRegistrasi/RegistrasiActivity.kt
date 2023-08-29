9fb862c98e9daf5030fea38d3913b7e2bd62eb20
reza <rezakurnia1902@gmail.com>
2023-08-29 07:56:47 +0700

    ui login regris

---
+++ app/src/main/java/com/example/cleaving/LoginRegistrasi/RegistrasiActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cleaving.LoginRegistrasi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cleaving.R
+
+class RegistrasiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrasi)
+    }
+}
