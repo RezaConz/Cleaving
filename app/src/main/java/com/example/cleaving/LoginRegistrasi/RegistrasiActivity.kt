def263fc80915dbdb83a5cc409ca78fcc06f7b89
reza <rezakurnia1902@gmail.com>
2023-08-29 20:26:50 +0700

    ui login regris

--- app/src/main/java/com/example/cleaving/LoginRegistrasi/RegistrasiActivity.kt
+++ app/src/main/java/com/example/cleaving/LoginRegistrasi/RegistrasiActivity.kt
@@ -3,10 +3,17 @@ package com.example.cleaving.LoginRegistrasi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.cleaving.R
+import com.example.cleaving.databinding.ActivityRegistrasiBinding
 
 class RegistrasiActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityRegistrasiBinding
+//    private database: Databa
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registrasi)
+        binding = ActivityRegistrasiBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
