1549c4ffa40e8b6fc2973f743b8f9b5fbd51aaba
rofif <rofifrahmanfuadi04@gmail.com>
2023-09-01 14:20:22 +0700

    SplashScreen

--- app/src/main/java/com/example/cleaving/OnBoardingPage.kt
+++ app/src/main/java/com/example/cleaving/OnBoardingPage.kt
@@ -1,11 +1,29 @@
 package com.example.cleaving
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.cleaving.LoginRegistrasi.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class OnBoardingPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_boarding_page)
+
+        val buttonWt1 = findViewById<Button>(R.id.ButtonOnBoarding)
+        buttonWt1.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    var firebashAuth = FirebaseAuth.getInstance()
+    override fun onStart() {
+        super.onStart()
+        if (firebashAuth.currentUser != null) {
+            startActivity(Intent(this, HomeFragment::class.java))
+        }
     }
 }
